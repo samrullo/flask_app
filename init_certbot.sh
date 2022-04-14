caeb54dcd36830d8bd2149b0c7f9193f5a202b36
samrullo <amrulloev.subhon@gmail.com>
2022-04-15 08:46:00 +0900

    flask with https

--- init_certbot.sh
+++ init_certbot.sh
@@ -2,8 +2,8 @@
 
 domains=(jspanda.club)
 rsa_key_size=4096
-data_path="./certbot"
-email="amrulloev.subhon@gmail.com" # Adding a valid address is strongly recommended
+data_path="./data/certbot"
+email="amrulloev.subhon.2016@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
